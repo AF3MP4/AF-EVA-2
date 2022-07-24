daa8a2c14ff7e5019afe77b1f4c05732ce1f65bb
AF3MP4 <103211743+AF3MP4@users.noreply.github.com>
2022-07-25 02:36:50 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('Support on YOUTUBE', url='https://youtube.com/channel/UCJpGpk7DKk-xxhoiWWUR_Sw')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Updates', url='https://t.me/af_updates')
@@ -88,7 +88,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('Support on YOUTUBE', url='https://youtube.com/channel/UCJpGpk7DKk-xxhoiWWUR_Sw')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('Updates', url='https://t.me/af_updates')
